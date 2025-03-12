1b1b8dbcaf86c47d0fbc5e665611f8e0e9217399
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-03-12 11:12:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
 DB_URI =  environ.get("DB_URI", "mongodb+srv://sumitsajwan135:gameno01@cluster0.ja0i0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = environ.get("DB_NAME", "Mong0Db")
+DB_NAME = environ.get("DB_NAME", "techvj")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
